5d656f0bf75b2f3b384172f573c45ccd8c1c0d14
Suissa <jnascimento@gmail.com>
2018-01-24 19:07:17 -0200

    iniciando getCandles

--- bleutrade.suissa.js
+++ bleutrade.suissa.js
@@ -146,22 +146,7 @@ const actions = {
 
 
     // api_query('getorderbook', callback, { market: market, period: period, count: count, lasthours: lasthours });
-
-  getcandles: async ({market = 'HTML_BTC', period = '30m', count = 1000, lasthours = 24}) =>{
-    const opt = getOptionsCandlesPublic('getorderbook',
-      {
-        market,
-        period, // 1m, 2m, 3m, 4m, 5m, 6m, 10m, 12m, 15m, 20m, 30m, 1h, 2h, 3h, 4h, 6h, 8h, 12h, 1d
-        count, // default: 1000, max: 999999
-        lasthours, // default: 24, max: 2160
-      }, {})
-      console.log('------------------------------------');
-    console.log(opt);
-      console.log('------------------------------------');
-    getResult(
-      await axios(opt))
-      },
-
+  
 
 // Market
 
@@ -389,11 +374,191 @@ const getPositiveChangeAndLastAboveAVG = async () => {
   }
 
 }
+
+const listCandle = [{
+  "TimeStamp": "2014-07-29 18:08:00",
+  "Quantity": 654971.69417461,
+  "Price": 0.00030,
+  "Total": 2,
+  "OrderType": "BUY"
+}, {
+    "TimeStamp": "2014-07-29 18:08:15",
+    "Quantity": 654971.69417461,
+    "Price": 0.00030,
+    "Total": 2,
+    "OrderType": "BUY"
+  }, {
+    "TimeStamp": "2014-07-29 18:08:35",
+    "Quantity": 120.00000000,
+    "Price": 0.00029,
+    "Total": 2,
+    "OrderType": "BUY"
+  }, {
+    "TimeStamp": "2014-07-29 18:08:45",
+    "Quantity": 120.00000000,
+    "Price": 0.00025,
+    "Total": 2,
+    "OrderType": "SELL"
+    // }, {
+  }, {
+    "TimeStamp": "2014-07-29 18:08:55",
+    "Quantity": 120.00000000,
+    "Price": 0.00022,
+    "Total": 2,
+    "OrderType": "SELL"
+  //   "TimeStamp": "2014-07-29 18:09:35",
+  //   "Quantity": 120.00000000,
+  //   "Price": 0.00000029,
+  //   "Total": 0.360234432,
+  //   "OrderType": "BUY"
+  // }, {
+  //   "TimeStamp": "2014-07-29 18:10:05",
+  //   "Quantity": 120.00000000,
+  //   "Price": 0.00000029,
+  //   "Total": 0.360234432,
+  //   "OrderType": "BUY"
+  }
+]
+// getcandles: async ({ market = 'HTML_BTC', period = '30m', count = 1000, lasthours = 24 }) => {
+//   const opt = getOptionsCandlesPublic('getorderbook',
+//     {
+//       market,
+//       period, // 1m, 2m, 3m, 4m, 5m, 6m, 10m, 12m, 15m, 20m, 30m, 1h, 2h, 3h, 4h, 6h, 8h, 12h, 1d
+//       count, // default: 1000, max: 999999
+//       lasthours, // default: 24, max: 2160
+//     }, {})
+//   console.log('------------------------------------');
+//   console.log(opt);
+//   console.log('------------------------------------');
+//   getResult(
+//     await axios(opt))
+// },
+
+// const toCandles = (obj, cur, i, list) => {
+//   const start = new Date( list[0].TimeStamp ).getTime()
+//   const minute = 60000
+//   const end = start - minute
+
+//   const time = new Date(cur.TimeStamp).getTime()
+
+//   if (time <= start && time >= end) {
+//     obj[1] = (!obj[1]) ? [cur] : [...obj[1], cur]
+
+//     return obj
+//   }
+
+//   return obj
+// }
+
+const getCandleType = (obj) =>  
+  (obj.Open === obj.Close)
+    ? 'neutral'
+    : (obj.Open < obj.Close)
+      ? 'green'
+      : 'red'
+const toCandle = (obj, cur, i, list) => {
+  if (!i) {
+    obj.TimeStamp = cur.TimeStamp
+    obj.TimeStampFirst = cur.TimeStamp
+    obj.TimeStampLast = cur.TimeStamp
+
+    obj.Buyers = (cur.OrderType.toLowerCase() === 'buy') ? 1 : 0
+    obj.Sellers = (cur.OrderType.toLowerCase() === 'sell') ? 1 : 0
+    obj.Last = cur.Price
+    obj.Open = cur.Price
+    obj.High = cur.Price
+    obj.Low = cur.Price
+    obj.Close = cur.Price
+    obj.Type = getCandleType(obj)
+
+    obj.AVGOpenClose = 0
+    obj.AVGHighLow = 0
+
+    obj.Volume = cur.Total
+    obj.BaseVolume = obj.Volume
+    obj.BaseVolumeAVG = obj.Volume
+    return obj
+  }
+  if (i + 1 === list.length) {
+    obj.Close = cur.Price
+    obj.Type = getCandleType(obj)
+    obj.TimeStampLast = cur.TimeStamp
+    obj.AVGOpenClose = ((obj.Open + obj.Close) / 2)
+  }
+
+  obj.High = (cur.Price > obj.High) ? cur.Price : obj.High
+  obj.Low = (cur.Price < obj.Low) ? cur.Price : obj.Low
+
+  obj.Buyers = (cur.OrderType.toLowerCase() === 'buy') ? obj.Buyers + 1 : obj.Buyers
+  obj.Sellers = (cur.OrderType.toLowerCase() === 'sell') ? obj.Sellers + 1 : obj.Sellers
+
+
+  obj.AVGHighLow = ((obj.High + obj.Low) / 2)
+
+  obj.Volume = obj.Volume + cur.Total
+  obj.BaseVolumeAVG = obj.Volume * obj.AVGHighLow
+  obj.BaseVolume = obj.Volume * obj.Last
+
+  return obj
+}
+
+// {
+//   "success": "true",
+//     "message": "",
+//       "result": [{
+//         "TimeStamp": "2014-07-31 10:15:00",
+//         "Open": "0.00000048",
+//         "High": "0.00000050",
+//         "Low": "0.00000048",
+//         "Close": "0.00000049",
+//         "Volume": "594804.73036048",
+//         "BaseVolume": "0.11510368"
+//       }, {
+//         "TimeStamp": "2014-07-31 10:00:00",
+//         "Open": "0.00000037",
+//         "High": "0.00000048",
+//         "Low": "0.00000035",
+//         "Close": "0.00000048",
+//         "Volume": "623490.14936407",
+//         "BaseVolume": "0.13101303"
+//       }
+//       ]
+// }
+const getCandles = (listCandle) => {
+// ({ market = 'HTML_BTC', size = 1, count = 1000, period = 24 }) => {
+  const list = listCandle.reverse()
+  const candleSize = 1
+  
+  // const candles = list.reduce(toCandle(candleSize), {})
+  const candles = list.reduce(toCandle, {})
+  console.log('------------------------------------');
+  console.log('candles: ', candles);
+  console.log('------------------------------------');
+}
+
+console.log(getCandles(listCandle))
+// const RS = (list, period) => 
+// const RSI = (period) => (list) => 100 – (100 / (1 + RS(list, period)))
+
 const Suisseba = {
   moreVolatile,
   getPositiveChangeAndLastAboveAVG
 }
 
+const sum = (x, y) => x + y
+const divide = (x, y) => x / y 
+const avg = (...list) => divide(list.reduce(sum, 0), list.length)
+const decimals = (decimal) => (x) => Number(Number(x).toFixed(decimal))
+
+// const with2decimals = decimals(2)
+// avg(2, 2, 3, 3, 10, 10, 10)
+// with2decimals(avg(2,2,3,3,10,10,10))
+
+// RSI = 100 – 100 / (1 + RS *)
+
+//   * RS = Média de X dias de alta / Média de X dias de baixa.
+
+// A quantidade de períodos padrão é 14, sendo que 9 e 28 também são comumente utilizados.Geralmente, mais períodos tendem a gerar dados mais precisos.
 
 const Bleutrade = (key, secret, requeue = 0) => {
   const config = {}
